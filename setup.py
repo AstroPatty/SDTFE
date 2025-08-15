73fe29efc48da092dc0b64e90b90c2d0817549e1
Patrick Wells <patrick.wells.95@gmail.com>
2025-08-15 10:52:15 -0500

    Fix build

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ class CMakeBuild(build_ext):
     def build_extension(self, ext):
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
         cmake_args = [
+            "-DCMAKE_EXE_LINKER_FLAGS='-lm'",
             "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + extdir,
             "-DPYTHON_EXECUTABLE=" + sys.executable,
             "-DCMAKE_POLICY_VERSION_MINIMUM=3.11",
